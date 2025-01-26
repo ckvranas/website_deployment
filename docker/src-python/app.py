b3767d7a1ac373596d64ebed1d800e4aa5c88855
ckvranas <christos.vran@gmail.com>
2025-01-26 11:10:15 +0100

    Added grafana configiration and more

--- docker/src-python/app.py
+++ docker/src-python/app.py
@@ -3,8 +3,16 @@ from flask import Flask
 app: Flask = Flask(__name__)
 
 @app.route('/')
-def index():
-    return 'Hello World!'
+def home_page():
+    return 'Helo world!'
+
+@app.route('/papers')
+def papers_page():
+    return 'Here are my papers!'
+
+@app.route('/patents')
+def patents_page():
+    return 'Here are my patents!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000, debug=True)    
